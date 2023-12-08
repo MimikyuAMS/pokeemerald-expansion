977cee811547e916bbdbf1094952920cdf141521
MimikyuAMS <aidensmith0206@gmail.com>
2023-12-07 20:52:49 -0400

    Aight Final Update....

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -659,7 +659,7 @@
 #define FLAG_CAUGHT_MEWTWO  0x267 // Unused Flag
 #define FLAG_CAUGHT_JIRACHI  0x268 // Unused Flag
 #define FLAG_HIDE_MEWTWO  0x269 // Unused Flag
-#define FLAG_UNUSED_0x26A  0x26A // Unused Flag
+#define FLAG_EXP_TOGGLE  0x26A // Unused Flag
 #define FLAG_UNUSED_0x26B  0x26B // Unused Flag
 #define FLAG_UNUSED_0x26C  0x26C // Unused Flag
 #define FLAG_UNUSED_0x26D  0x26D // Unused Flag
